43ddb155b21b971737ab285d87ff3e7b1cc14c6a
sergisalv <sergisalv@gmail.com>
2024-08-22 22:48:34 +0200

    Comenzando proyecto

---
+++ src/main/java/com/example/gestionclientes/GestionclientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionclientes/GestionclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
